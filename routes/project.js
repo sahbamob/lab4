4f158ba93586219ba40cc3361488db6d8782e6ff
Sahba Mobini <smobini19@gmail.com>
2020-01-29 11:08:23 -0800

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,12 @@
+
+/**
+ * Controller for project pages
+ */
+
+ exports.viewProject = function(req, res) {
+ 	var name = req.params.name;
+ 	console.log("The project name is: " + name);
+ 	res.render('project', {
+ 		'projectName': name
+ 	});
+ }
